fb925cf32369314b9f66a9c028d39b694b6d0843
Nathanial Howard <nathanial55@gmail.com>
2023-09-29 19:55:02 +0100

    Working config

--- passthrough-start.sh
+++ passthrough-start.sh
@@ -16,7 +16,7 @@ cleanup () {
 	#sudo -u pi -E /usr/share/xrandr-config.sh
 
 	# Return CPU power management to default
-	#pstate-frequency --set -p auto -n 50
+	pstate-frequency --set -p auto -n 50
 
 	if [[ $TMP_PARAMS == *'--keep-hugepages'* ]]
 	then
@@ -61,10 +61,11 @@ then
 	exit 0
 fi
 
+# Create network bridge
+./create_bridge.sh
 
 # CPU governor settings (keep CPU frequency up, might not work on older CPUs - use cpupower for those)
-#pstate-frequency --set -p max
-
+pstate-frequency --set -p max
 
 # Hugepages config
 export HUGEPAGES=11000
@@ -95,27 +96,6 @@ else
     echo "Hugepages already found, let's use those!"
 fi
 
-# Move polybar to secondary screen
-#killall polybar
-
-# Monitor config
-if [[ $TMP_PARAMS == *'--display'* ]]
-then
-	ddcutil --display 1 setvcp 60 1
-	xrandr --output HDMI-A-0 --off
-fi
-
-# Start synergy for mouse and keyboard sharing
-echo "Starting SynergyC..."
-#synergyc 10.0.0.251
-#setxkbmap de # Required because synergy doesn't load default for whatever reason. Not needed on US keyboards.
-
-# Start Scream-Pulse receiver for audio over the NAT network
-echo "Starting sound receiver..."
-#sudo -u pi -E sh -c "scream-pulse -i winbr0" &
-
-#sudo -u pi -E sh -c "env SYSTRAY_MON=\"DisplayPort-0\" /home/pi/.config/polybar/bar_launch.sh > /dev/null 2>&1 &disown"
-
 # Taskset (Move all current processes to unused cores)
 # Done last, so it can move synergy and scream-pulse as well
 #cset shield --kthread on --cpu 1-5,7-11
@@ -139,8 +119,8 @@ sysctl -w kernel.watchdog=0
 # Start VM via virt-manager
 echo "VM starting..."
 # Remove existing VM
-#virsh undefine --nvram win11
-#virsh define win11-working.xml
+virsh undefine --nvram win11
+virsh define win11-working.xml
 virsh start win11
 echo
 

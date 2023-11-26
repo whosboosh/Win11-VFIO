4fc16233878c35db1e3b8c74ddff0f62c8791178
Nathanial Howard <nathanial55@gmail.com>
2023-11-26 01:53:38 +0000

    Put watchman dongles back as pass through

--- qemu_fifo.sh
+++ qemu_fifo.sh
@@ -2,7 +2,7 @@
 
 arg="$1"
 shift
-if [ $arg == "--cleanup" ]; then
+if [ "$arg" == "--cleanup" ]; then
 	echo "Cleanup qemu fifo"
 	grep ahci /proc/interrupts | cut -d ":" -f 1 | while read -r i; do echo $i; MASK=0; echo $MASK > /proc/irq/$i/smp_affinity_list; done
 	exit 0

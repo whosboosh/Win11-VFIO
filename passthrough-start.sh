6023528131ea7905a6c96db74583baa72783756a
Nathanial Howard <nathanial55@gmail.com>
2024-02-05 22:52:28 +0000

    Win10 config

--- passthrough-start.sh
+++ passthrough-start.sh
@@ -1,9 +1,24 @@
 #!/bin/bash
 
-if [[ $UID -ne 0 ]]; then
-	sudo -E bash $0 "$@"
-	exit $?
-fi
+name=win10
+redefine=
+optimisations=
+while getopts "o:rn:c:" arg; do
+  case $arg in
+    o)
+      optimisations=true
+	  shift
+      ;;
+    r)
+      redefine=true
+	  shift
+      ;;
+    n)
+      name=${OPTARG}
+	  shift
+      ;;
+  esac
+done
 
 # Necessary for parameter usage in cleanup()
 export TMP_PARAMS="$*"
@@ -30,7 +45,7 @@ cleanup () {
 	echo fff > /sys/devices/virtual/workqueue/writeback/cpumask
 	echo 950000 > /proc/sys/kernel/sched_rt_runtime_us
 	sysctl vm.stat_interval=1
-        sysctl -w kernel.watchdog=1
+	sysctl -w kernel.watchdog=1
 
 	sleep 2
 	./qemu_fifo.sh --cleanup
@@ -44,50 +59,53 @@ then
 	exit 0
 fi
 
-# CPU governor settings (keep CPU frequency up, might not work on older CPUs - use cpupower for those)
-pstate-frequency --set -p max
+if [ ! -z $optimisations ]; then
+	# CPU governor settings (keep CPU frequency up, might not work on older CPUs - use cpupower for those)
+	pstate-frequency --set -p max
 
-# Hugepages config
-export HUGEPAGES=16000
+	# Hugepages config
+	export HUGEPAGES=16000
 
-# Note that allocating hugepages after boot has a chance to fail. If continuous memory
-# cannot be allocated, a reboot will be required.
-export STARTING_HUGEPAGES="$(cat /proc/sys/vm/nr_hugepages)"
-if [[ "$STARTING_HUGEPAGES" -lt "$HUGEPAGES" ]]; then
-    # Drop caches and compact memory to free up continuous memory for huge pages
-    echo 3 > /proc/sys/vm/drop_caches
-    echo 1 > /proc/sys/vm/compact_memory
+	# Note that allocating hugepages after boot has a chance to fail. If continuous memory
+	# cannot be allocated, a reboot will be required.
+	export STARTING_HUGEPAGES="$(cat /proc/sys/vm/nr_hugepages)"
+	if [[ "$STARTING_HUGEPAGES" -lt "$HUGEPAGES" ]]; then
+		# Drop caches and compact memory to free up continuous memory for huge pages
+		echo 3 > /proc/sys/vm/drop_caches
+		echo 1 > /proc/sys/vm/compact_memory
 
 
-    echo "Allocating $HUGEPAGES hugepages..."
-    echo "$HUGEPAGES" > /proc/sys/vm/nr_hugepages
-    ALLOC_PAGES="$(cat /proc/sys/vm/nr_hugepages)"
+		echo "Allocating $HUGEPAGES hugepages..."
+		echo "$HUGEPAGES" > /proc/sys/vm/nr_hugepages
+		ALLOC_PAGES="$(cat /proc/sys/vm/nr_hugepages)"
 
-    if [[ "$ALLOC_PAGES" -lt "$HUGEPAGES" ]]; then
-      echo
-      echo 'Not able to allocate hugepages'
-      echo "Current max: $ALLOC_PAGES"
-      echo
+		if [[ "$ALLOC_PAGES" -lt "$HUGEPAGES" ]]; then
+		echo
+		echo 'Not able to allocate hugepages'
+		echo "Current max: $ALLOC_PAGES"
+		echo
 
-      cleanup
-      exit 1
-    fi
-else
-    echo "Hugepages already found, let's use those!"
+		cleanup
+		exit 1
+		fi
+	else
+		echo "Hugepages already found, let's use those!"
+	fi
+
+	#echo "Performing minor optimizations prior to launch..."
+	sysctl vm.stat_interval=120
+	sysctl -w kernel.watchdog=0
+	sysctl kernel.sched_rt_runtime_us=1000000
 fi
-
-#echo "Performing minor optimizations prior to launch..."
-#sysctl vm.stat_interval=120
-#sysctl -w kernel.watchdog=0
-#sysctl kernel.sched_rt_runtime_us=1000000
 
 # Start VM via virt-manager
 echo "VM starting..."
+if [ ! -z $redefine ]; then
 # Remove existing VM
-virsh undefine --nvram win11
-virsh define win11-working-lookingglass-7950x3d.xml
-virsh start win11
-echo
+	virsh undefine --nvram $name
+	virsh define $name-working-lookingglass-7950x3d.xml
+fi
+virsh start $name
 
 # Start looking glass
 sudo -u nate ./start-lookingglass.sh &
